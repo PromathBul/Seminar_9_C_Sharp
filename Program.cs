ded2e59baa56c2de898e066f1cae4d7e229e7733
Dmitry_Tikhonov <tdmv1209@gmail.com>
2022-12-25 11:35:27 +0300

    Семинар 9

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+void SeriesNumsFromMToN(int m, int n)
+{
+    if(m < n) 
+    {
+        SeriesNumsFromMToN(m, n - 1);
+        Console.Write(n + " ");
+    }
+    
+    if(n < m)
+    {
+        Console.Write(m + " ");
+        SeriesNumsFromMToN(m - 1, n); 
+    }
+
+    if(n == m) Console.Write(m + " ");
+}
+
+SeriesNumsFromMToN(5, 2);
+*/
+
+double Power(int basic, int degree)
+{
+    if(degree > 0) return Power(basic, degree - 1) * basic;
+    else if(degree < 0) return Power(basic, degree + 1) / basic;
+    else return 1;
+}
+Console.WriteLine(Power(2, -3));
